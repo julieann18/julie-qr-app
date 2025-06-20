b19350815ff73da638f314374dfb9e95faf4d0c2
vince0415 <sagcalvince0415@gmail.com>
2025-06-20 15:49:06 +0800

    update student info controller

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
   <v-app dark>
+    <!-- Left Navigation Drawer -->
     <v-navigation-drawer
       v-model="drawer"
       :mini-variant="miniVariant"
@@ -24,66 +25,63 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
+
+    <!-- App Bar -->
     <v-app-bar
       :clipped-left="clipped"
       fixed
       app
     >
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+
+      <v-btn icon @click.stop="miniVariant = !miniVariant">
+        <v-icon>{{ miniVariant ? 'mdi-chevron-right' : 'mdi-chevron-left' }}</v-icon>
       </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
+
+      <v-btn icon @click.stop="clipped = !clipped">
         <v-icon>mdi-application</v-icon>
       </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
+
+      <v-btn icon @click.stop="fixed = !fixed">
         <v-icon>mdi-minus</v-icon>
       </v-btn>
+
       <v-toolbar-title>{{ title }}</v-toolbar-title>
       <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
+
+      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
         <v-icon>mdi-menu</v-icon>
       </v-btn>
     </v-app-bar>
+
+    <!-- Main Content -->
     <v-main>
-      <v-container>
+      <v-container fluid>
         <Nuxt />
       </v-container>
     </v-main>
+
+    <!-- Right Navigation Drawer -->
     <v-navigation-drawer
       v-model="rightDrawer"
       :right="right"
       temporary
       fixed
+      app
     >
       <v-list>
-        <v-list-item @click.native="right = !right">
+        <v-list-item @click="right = !right">
           <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
+            <v-icon light>mdi-repeat</v-icon>
           </v-list-item-action>
           <v-list-item-title>Switch drawer (click me)</v-list-item-title>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+
+    <!-- Footer -->
+    <v-footer :fixed="fixed" app>
+      <span>&copy; {{ currentYear }}</span>
     </v-footer>
   </v-app>
 </template>
@@ -96,6 +94,10 @@ export default {
       clipped: false,
       drawer: false,
       fixed: false,
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js',
       items: [
         {
           icon: 'mdi-apps',
@@ -106,12 +108,18 @@ export default {
           icon: 'mdi-chart-bubble',
           title: 'Inspire',
           to: '/inspire'
+        },
+         {
+          icon: 'mdi-chart-bubble',
+          title: 'scanner',
+          to: '/scanner'
         }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      ]
+    }
+  },
+  computed: {
+    currentYear () {
+      return new Date().getFullYear()
     }
   }
 }
